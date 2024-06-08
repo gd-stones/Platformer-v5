93bd484bb6d9397742d52f106e5ed4a6679138d5
gd-stones <ngducviet2105@gmail.com>
2024-06-09 03:43:21 +0700

    add package level selection

---
+++ Assets/2. Scripts/UI/Test.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Get the default value : " + PlayerPrefs.GetInt("NEVERCreateThisKeyBefore"));
+    }
+}
